0c79e23b2a41a142891f496536f4d72a5779719c
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-05 14:19:25 -0500

    updated models

--- models/workouts.js
+++ models/workouts.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for workout"
-  },
-  value: {
-    type: Number,
-    required: "Enter an time"
-  },
-  date: {
+  day: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  exercises: [{
+    type: String,
+    name: String,
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
+    distance: Number
+  }],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
