99854e55ceb25cd8cc73feb7654513e362ad1c9f
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-07 16:48:35 -0500

    updated server and fix api file

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ app.use(require("./routes/api.js"));
 // })
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port http://localhost:${PORT}`);
 })
