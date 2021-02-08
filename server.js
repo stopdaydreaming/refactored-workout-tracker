a7d467b1f5860f67933d8dd1e59a7470046d8db5
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-08 00:59:53 -0500

    fixed error in serverjs

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost/workout", { 
     useNewURLParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
 });
 
 //routes
