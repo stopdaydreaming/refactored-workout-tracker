802bf14802a1ff612e77627c8c3a17c0f405597c
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-08 00:40:13 -0500

    fixed error in serverjs

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost/workout", {
 
 //routes
 app.use(require(apiController));
-app.use(require(viewsController);)
+app.use(require(viewsController));
 
 app.listen(PORT, () => {
     console.log(`App running on port http://localhost:${PORT}`);
