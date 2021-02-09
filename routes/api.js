5d344420c523a7063de45972b6555e5a32272e43
stopdaydreaming <hello@stopdaydreaming.com>
2021-02-08 21:31:24 -0500

    workout lowercase

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 
 //get route of last workout
 router.get("/api/workouts", (req, res) => {
